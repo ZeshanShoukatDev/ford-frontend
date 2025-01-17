9b6af95aec31527a148fa931cffa3f95fd85993d
Ali Azhar <aliazhar@vmi2002743.contaboserver.net>
2025-01-17 18:06:18 +0100

    Centered Logo aRemoved Dark  Mobile screens

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,27 +21,34 @@ const props = defineProps({
   >
     <div class="container flex h-16 items-center justify-between px-4">
       <!-- Placeholder for alignment -->
-      <div class="flex-1"></div>
+      <div></div>
 
       <!-- Logo in the center -->
       <div class="flex justify-center items-center h-[80px]">
         <img
           src="/logo1.png"
           alt="Ford Logo"
-          class="object-contain w-[120px] h-[120px] sm:w-[1500px] sm:h-[150px] lg:w-[200px] lg:h-[200px]"
+          class="object-contain w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] lg:w-[200px] lg:h-[200px]"
         />
       </div>
 
       <!-- Dark mode toggle button -->
       <button
         @click="toggleDarkMode"
-        class="flex-1 flex justify-end items-center space-x-2 p-2 rounded-full text-black hover:bg-black hover:text-white dark:text-gray-400 font-bold dark:hover:bg-white dark:hover:text-black transition-colors duration-200"
+        class="flex items-center space-x-2 p-2 rounded-full text-black hover:bg-black hover:text-white dark:text-gray-400 font-bold dark:hover:bg-white dark:hover:text-black transition-colors duration-200"
       >
-        <Moon v-if="!isDarkMode" class="h-5 w-5" />
-        <Sun v-else class="h-5 w-5" />
-        <span class="text-sm">
-          {{ isDarkMode ? "Switch to Light" : "Switch to Dark" }}
-        </span>
+        <!-- Icon only on mobile screens -->
+        <Moon v-if="!isDarkMode" class="h-5 w-5 sm:hidden" />
+        <Sun v-else class="h-5 w-5 sm:hidden" />
+
+        <!-- Icon + Text on larger screens -->
+        <div class="hidden sm:flex items-center space-x-2">
+          <Moon v-if="!isDarkMode" class="h-5 w-5" />
+          <Sun v-else class="h-5 w-5" />
+          <span class="text-sm">
+            {{ isDarkMode ? "Switch to Light" : "Switch to Dark" }}
+          </span>
+        </div>
       </button>
     </div>
   </header>
