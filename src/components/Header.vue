c0dc205ae723d7b6e3fea73b5442032cd9575d14
Ali Azhar <aliazhar@vmi2002743.contaboserver.net>
2025-01-20 12:37:34 +0100

    Logo is Centered

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="w-full border-b">
-    <div class="flex h-16 items-center justify-center lg:ml-16">
+    <div class="flex h-16 items-center justify-center">
       <!-- Logo in the center -->
       <div class="flex justify-center items-center h-[80px]">
         <img
           src="/logo1.png"
           alt="Ford Logo"
-          class="object-contain w-[120px] h-[120px] sm:w-[150px] sm:h-[150px] lg:w-[200px] lg:h-[200px]"
+          class="object-contain w-[150px] h-[150px] sm:w-[180px] sm:h-[180px] lg:w-[200px] lg:h-[200px]"
         />
       </div>
     </div>
