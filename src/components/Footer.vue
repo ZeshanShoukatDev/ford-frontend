5dee7da4fd53f60c4a82bda1cd4dcbafa185af42
aliazhar5 <aalliiazhar@gmail.com>
2025-07-08 18:39:31 +0200

    Removed Visit Website button for Lawhorn Ford Sales

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -33,6 +33,7 @@
         </p>
         <!-- Use anchor tag for navigation -->
         <a
+          v-if="dealer.name !== 'Lawhorn Ford Sales'"
           :href="dealer.website_url"
           target="_blank"          
           class="w-full mt-2 px-4 py-2 rounded transition-colors duration-200 text-sm border bg-white text-[#1C79C4] border-[#1C79C4] hover:text-white hover:bg-[#1C79C4]"
@@ -43,7 +44,7 @@
     </div>
     <div class="mt-20 border-t pt-4 text-center text-sm text-gray-600">
       <p>
-        &copy; {{ new Date().getFullYear() }} Site Designed By November Digital and Associates.
+        &copy; Copyright {{ new Date().getFullYear() }} Site Designed By November Digital and Associates.
       </p>
     </div>
   </footer>
